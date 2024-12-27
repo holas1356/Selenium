aea98e5f1ab67baa7ce8610c9efb28f36466f1f3
holas1356 <ruthbustamante075@gmail.com>
2024-12-27 17:06:36 -0500

    Add GitHub Actions pipeline to run automated tests and upload results to Xray

--- utils/browser_utils.py
+++ utils/browser_utils.py
@@ -4,7 +4,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.chrome.options import Options
 import time
+
 
 def init_driver():
     service = Service(ChromeDriverManager().install())
@@ -21,4 +23,20 @@ def wait_for_element(driver, locator, condition=EC.presence_of_element_located, 
         except StaleElementReferenceException:
             time.sleep(1) 
     raise Exception(f"Elemento no encontrado o inestable: {locator}")
+
+
+
+
+def get_chrome_driver():
+    options = Options()
+    options.add_argument('--headless')  
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--remote-debugging-port=9222')
+    
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+
 
