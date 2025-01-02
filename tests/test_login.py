87023579888a3849b639c711b2295911813f1976
holas1356 <ruthbustamante075@gmail.com>
2025-01-02 09:06:30 -0500

    Add GitHub Actions pipeline to run automated tests and upload results to Xray

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,36 +12,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def setup():
+@pytest.fixture
+def driver_setup():
     driver = init_driver()
-
     base_url = os.getenv("BASE_URL")
-    print (base_url)
     driver.get(base_url)
-    
     yield driver
     driver.quit()
 
-def test_login_successful(setup):
+def test_login_successful(driver_setup):
     email = os.getenv("EMAIL")
     password = os.getenv("PASSWORD")
-    login_page = LoginPage(setup)
+    login_page = LoginPage(driver_setup)
     login_page.login(email, password)
-    assert "Home" in setup.title, "La autenticación no fue exitosa. Título encontrado: " + setup.title
-    import_page = ImportPage(setup)
+    assert "Home" in driver_setup.title, "La autenticación no fue exitosa. Título encontrado: " + driver_setup.title
+
+    import_page = ImportPage(driver_setup)
     import_page.go_to_import_page()
     import_page.verify_import_page()
     import_page.select_garmy_option()
 
     import_page.upload_csv_file("tests/documents/test_GARMY_Luisa_3.csv")
     time.sleep(30)
-    assert "Imports" in setup.title, "La autenticación no fue exitosa. Título encontrado: " + setup.title
-    logistics_page = LogisticsPage(setup)
+    assert "Imports" in driver_setup.title, "La autenticación no fue exitosa. Título encontrado: " + driver_setup.title
+
+    logistics_page = LogisticsPage(driver_setup)
     logistics_page.go_to_logistics_page()
     logistics_page.verify_inventory_master_page()
     logistics_page.select_tactical_type()
     logistics_page.enter_nomenclature("Test")
     time.sleep(20)
-    assert "Inventory" in setup.title, "La autenticación no fue exitosa. Título encontrado: " + setup.title
-    
-   
+    assert "Inventory" in driver_setup.title, "La autenticación no fue exitosa. Título encontrado: " + driver_setup.title
