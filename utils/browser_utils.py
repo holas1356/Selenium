77f7cc22d39107f163cb552c17e0e6e277f68514
holas1356 <ruthbustamante075@gmail.com>
2025-01-02 08:21:24 -0500

    Add GitHub Actions pipeline to run automated tests and upload results to Xray

--- utils/browser_utils.py
+++ utils/browser_utils.py
@@ -12,6 +12,7 @@ def init_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless") 
     chrome_options.add_argument("--no-sandbox") 
+    chrome_options.add_argument('--invalid-argument')
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--remote-debugging-port=9222")  
     chrome_options.add_argument('--disable-software-rasterizer')
